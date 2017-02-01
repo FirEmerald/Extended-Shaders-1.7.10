b018b9c0419d5680c7a996d4b87d853f5160e2fe
FirEmerald <mccaslinwalt@gmail.com>
2017-01-31 23:50:49 -0600

    update to 2.0
    update API to 2.0. Remove support for multiple render passes.

--- src/main/java/extendedshaders/api/ShaderEvent.java
+++ src/main/java/extendedshaders/api/ShaderEvent.java
@@ -20,7 +20,7 @@ public abstract class ShaderEvent extends Event
 	/**
 	 * This event is fired before the sky is rendered.<br>
 	 * <br>
-	 * {@link #partialTicks} is the partial-tick time<br>
+	 * {@link #partialTicks} is the partial-tick time.<br>
 	 * <br>
 	 * This event is {@link Cancelable}.<br>
 	 * If the event is canceled, the sky will not be rendered.<br>
@@ -43,31 +43,6 @@ public abstract class ShaderEvent extends Event
 	}
 
 	/**
-	 * This event is fired at the beginning of a render pass.<br>
-	 * <br>
-	 * {@link #partialTicks} is the partial-tick time<br>
-	 * {@link #renderPass} is the render pass<br>
-	 * <br>
-	 * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
-	 **/
-	public static abstract class RenderPass extends ShaderEvent
-	{
-		public final int renderPass;
-		/** ONLY USED BY FORGE!!! **/
-		public RenderPass()
-		{
-			super();
-			renderPass = 0;
-		}
-		
-		public RenderPass(float partialTicks, int renderPass)
-		{
-			super(partialTicks);
-			this.renderPass = renderPass;
-		}
-	}
-
-	/**
 	 * This event is fired before shaders are run<br>
 	 * <br>
 	 * {@link #partialTicks} is the partial-tick time<br>
@@ -75,7 +50,7 @@ public abstract class ShaderEvent extends Event
 	 * <br>
 	 * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
 	 **/
-	public static class Start extends RenderPass
+	public static class Start extends ShaderEvent
 	{
 		/** ONLY USED BY FORGE!!! **/
 		public Start()
@@ -85,29 +60,7 @@ public abstract class ShaderEvent extends Event
 		
 		public Start(float partialTicks)
 		{
-			super(partialTicks, 1);
-		}
-	}
-
-	/**
-	 * This event is fired when settings are reset between render passes.<br>
-	 * <br>
-	 * {@link #partialTicks} is the partial-tick time<br>
-	 * {@link #renderPass} is the render pass<br>
-	 * <br>
-	 * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
-	 **/
-	public static class Loop extends RenderPass
-	{
-		/** ONLY USED BY FORGE!!! **/
-		public Loop()
-		{
-			super();
-		}
-		
-		public Loop(float partialTicks, int renderPass)
-		{
-			super(partialTicks, renderPass);
+			super(partialTicks);
 		}
 	}
 
